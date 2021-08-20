98cb3791804e2d6782b38ffd7aa746dc6f591883
Sri Aspari <mail@siarie.me>
2021-08-20 18:55:27 +0700

    feat: add golang support

--- elisp/languages.el
+++ elisp/languages.el
@@ -4,6 +4,7 @@
  'load-path
  (expand-file-name "languages" (file-name-directory load-file-name)))
 
+(require 'lang-go)
 (require 'lang-php)
 (require 'lang-python)
 
---
+++ elisp/languages/lang-go.el
@@ -0,0 +1,21 @@
+;;; lang-go.el --- Golang Mode
+
+;;; Code:
+
+;; make sure go-mode installed
+(el-get-bundle go-mode)
+
+(defun lang/go-mode--setup ()
+  "Golang mode setup."
+  (local-set-key (kbd "C-c C-f") 'gofmt)
+  ;; format before save
+  (add-hook 'before-save-hook 'gofmt-before-save))
+
+
+;; go-mode hook
+(add-hook 'go-mode-hook 'lang/go-mode--setup)
+
+(provide 'lang-go)
+
+;;; lang-go.el ends here
+
