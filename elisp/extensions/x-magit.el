f6324f629869fa2714b340f9d3997f34bd7e1dc4
Sri Aspari <mail@siarie.me>
2021-09-27 07:27:44 +0700

    fix(magit): Doesn't ask for ssh password on windows

--- elisp/extensions/x-magit.el
+++ elisp/extensions/x-magit.el
@@ -8,6 +8,10 @@
   (setq transient-default-level 5
 	    magit-completing-read-function 'ivy-completing-read))
 
+;; Use git-gui--askpass on windows
+(when (eq system-type 'windows-nt)
+  (setenv "GIT_ASKPASS" "git-gui--askpass")
+  (setenv "SSH_ASKPASS" "git-gui--askpass"))
 
 (provide 'x-magit)
 
