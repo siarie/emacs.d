29911b6c1cf8f01c2554a36e0e3adcf85955f004
Sri Aspari <mail@siarie.me>
2021-10-02 10:15:20 +0700

    fix: auto-fill-column-mode doesn't work properlly
    auto-fill-mode only work on comment. it's not work on text-mode or
    fundamental-mode this because `comment-auto-fill-only-comments` is
    enable globally.
    
    Signed-off-by: Sri Aspari <mail@siarie.me>

--- elisp/core/core-editor.el
+++ elisp/core/core-editor.el
@@ -39,10 +39,12 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; auto fill column
-(auto-fill-mode 1)
+(setq-default auto-fill-function 'do-auto-fill)
 (global-display-fill-column-indicator-mode)
 (setq fill-column 72)
-(setq comment-auto-fill-only-comments t)
+(add-hook 'prog-mode-hook (lambda ()
+                            (setq comment-auto-fill-only-comments t)
+                            (auto-fill-mode 1)))
 
 ;; disable ring bell
 (setq ring-bell-function 'ignore)
