5161b54cfbe57c8fdb45e85f0c2a1d2d2a056442
Sri Aspari <mail@siarie.me>
2021-11-23 17:05:01 +0700

    lang(js): Don't start flymake-eslint by default
    Signed-off-by: Sri Aspari <mail@siarie.me>

--- elisp/languages/lang-javascript.el
+++ elisp/languages/lang-javascript.el
@@ -7,10 +7,9 @@
 (defun lang/js-mode--setup ()
   "Javascript mode hook."
   (eglot-ensure)
-  (setq-local eglot-stay-out-of '(flymake))
-  (add-hook 'flymake-diagnostic-functions 'eglot-flymake-backend nil t)
-  (local-set-key (kbd "C-c C-f") #'eglot-format-buffer)
-  (flymake-eslint-enable))
+  ;; (setq-local eglot-stay-out-of '(flymake))
+  ;; (add-hook 'flymake-diagnostic-functions 'eglot-flymake-backend nil t)
+  (local-set-key (kbd "C-c C-f") #'eglot-format-buffer))
   ;; (add-hook 'before-save-hook #'eglot-format-buffer))
 
 (add-hook 'typescript-mode-hook #'lang/js-mode--setup)
